b03f25983443ef05a8a71e13407f82d9371b6713
mdus <marcin.dus@nokia.com>
2019-10-28 14:33:01 +0100

    Saddle problem solved

--- saddle.cpp
+++ saddle.cpp
@@ -1,4 +1,4 @@
-
+#include <algorithm>
 #include <gtest/gtest.h>
 #include <vector>
 #include <tuple>
@@ -12,34 +12,43 @@ using Table = vector<vector<int>>;
 int getMaxIndex(vector<int> v);
 int getMinIndex(vector<int> v);
 vector<int> getCol(int , Table );
+vector<pair<int,int>> answer(Table v);
 
 constexpr auto SIZE = 5u;
 
-vector<pair<int,int>> answer(Table v) { 
+vector<pair<int,int>> answer(Table v) {
 
     vector<pair<int,int>> result;
-    //fix that!
-    for(int i = 0 ; i < SIZE ; i++) {
-        if( max_element(v[i].begin(), v[i].end())
-        == min_element(getCol(i, v).begin(), getCol(i, v).end()) ) {
-            result.push_back(make_pair(getMaxIndex(v[i]) , getMinIndex(getCol(i, v))));
-            cout << getMaxIndex(v[i])  << " " << getMinIndex(getCol(i, v)) << endl;
+    vector<int> temporaryCol;
+    int maxIndex;
+    int minIndex;
+
+    for(unsigned int i = 0 ; i < SIZE ; i++) {
+        maxIndex = getMaxIndex(v[i]);
+        temporaryCol = getCol(maxIndex,v);
+        minIndex = getMinIndex(temporaryCol);
+
+        cout <<  v[minIndex][i] << " == " <<  temporaryCol[minIndex] << endl;
+        if(v[minIndex][i] == temporaryCol[minIndex]) {
+            result.push_back(make_pair(minIndex, maxIndex));
         }
+
     }
     return result;
 }
 
 
 vector<int> getCol(int i, Table t) {
-
     vector<int> v;
-    for(int k = 0 ; k < SIZE ; k++) {
+    for(unsigned int k = 0 ; k < SIZE ; k++) {
             v.push_back(t[k][i]);
             }
 
-
     return v;
 }
+
+
+
 
 int getMaxIndex(vector<int> v) {
     return distance(v.begin(), max_element(v.begin(), v.end()));
@@ -51,41 +60,67 @@ int getMinIndex(vector<int> v) {
 
 
 
-TEST(Hiker, Life_the_universe_and_everything){
+TEST(saddle, findSaddlePointPlacedNotOnDiagonal){
 
-Table table =        {{0, 0, 4, 0, 0},
-        {1, 2, 3, 2, 1},
-        {0, 0, 5, 0, 0},
-        {0, 0, 6, 0, 0},
-        {0, 0, 7, 0, 0}};
+Table table =  {{0, 0, 4, 0, 0},
+                {1, 2, 3, 2, 1},
+                {0, 0, 5, 0, 0},
+                {0, 0, 6, 0, 0},
+                {0, 0, 7, 0, 0}};
+
+vector<pair<int,int>> result;
+result.push_back(make_pair(1,2));
+ASSERT_EQ(result, answer(table));
+}
+
+
+
+
+
+TEST(saddle, findSaddlePointPlacedOnDiagonal){
+
+Table table =  {{0, 0, 4, 0, 0},
+                {1, 2, 5, 2, 1},
+                {0, 0, 3, 0, 0},
+                {0, 0, 6, 0, 0},
+                {0, 0, 7, 0, 0}};
 
 vector<pair<int,int>> result;
 result.push_back(make_pair(2,2));
 ASSERT_EQ(result, answer(table));
 }
 
-TEST(Hiker, getCol) {
+
+TEST(saddle, getCol) {
 
 
-       Table table =        {{0, 0, 4, 0, 0},
-        {1, 2, 3, 2, 1},
-        {0, 0, 5, 0, 0},
-        {0, 0, 6, 0, 0},
-        {0, 0, 7, 0, 0}};
+Table table =  {{0, 0, 4, 0, 0},
+                {1, 2, 5, 2, 1},
+                {0, 0, 3, 0, 0},
+                {0, 0, 6, 0, 0},
+                {0, 0, 7, 0, 0}};
 
     vector<int> col = {0,1,0,0,0};
     ASSERT_EQ(col , getCol(0,table));
+    col = {4,5,3,6,7};
+    ASSERT_EQ(col , getCol(2,table));
+
 }
 
 
-TEST(Hiker, getMaxFromVector) {
+TEST(saddle, getMaxFromVector) {
     vector<int> vec = {0,22,1};
     ASSERT_EQ(1, getMaxIndex(vec));
 }
 
-TEST(Hiker, getMinFromVector) {
+TEST(saddle, getMinFromVector) {
     vector<int> vec = {0,22,1};
     ASSERT_EQ(0, getMinIndex(vec));
 }
+
+
+
+
+
 
 
